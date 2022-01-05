c4d9de02536fa7a4f84924ad8cb1e4859609fbb1
Bacoq4 <53489761+Bacoq4@users.noreply.github.com>
2022-01-05 23:02:16 +0300

    Add files via upload

---
+++ ExecuteRefCodeAlways.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class ExecuteRefCodeAlways : MonoBehaviour
+{
+    public HairHolder hairHolder;
+
+    // Update is called once per frame
+    void Update()
+    {
+        hairHolder.changeHeightOfStacks();
+    }
+}
---
+++ HairHead.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairHead : HairBase
+{
+    void LateUpdate()
+    {
+        Vector3 correctedPos = new Vector3(nextStack.position.x, nextStack.position.y, transform.position.z);
+        transform.position = correctedPos;
+    } 
+    protected override void Update()
+    {
+
+    }
+}
